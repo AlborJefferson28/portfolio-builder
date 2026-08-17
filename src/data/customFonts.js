6a4063fba4074e8ddeeb660fce176c91022cde0e
AlborJefferson28 <jeffersonalbor28@gmail.com>
2026-08-17 17:19:57 -0500

    feat: agregar registro de plantillas de portfolio

---
+++ src/data/customFonts.js
@@ -0,0 +1,7 @@
+export const CUSTOM_FONTS = [
+  'Fraunces', 'Playfair Display', 'DM Serif Display', 'Cormorant Garamond', 'Libre Baskerville',
+  'Archivo Black', 'Big Shoulders Display', 'Space Grotesk', 'Bricolage Grotesque', 'Sora',
+  'Inter', 'Work Sans', 'Nunito Sans', 'IBM Plex Sans', 'Source Sans 3',
+  'Karla', 'Manrope', 'Public Sans', 'Archivo', 'Outfit',
+  'JetBrains Mono', 'Space Mono', 'IBM Plex Mono', 'Fira Code',
+];
